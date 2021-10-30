4a5a7cfae0456cee5ce826b7f449824f6116c895
Deni <denisamirza1@gmail.com>
2021-10-30 23:06:56 +0300

    Establish database connection

--- backend/server.js
+++ backend/server.js
@@ -23,4 +23,31 @@ app.get('/signin', (req, res) => {
     res.json({message: "signin"})
 })
 
+const userSchema = {
+    username: String,
+    password: String,
+};
+
+const user = mongoose.model(
+    "Users",
+    userSchema,
+    "users"
+);
+
+user.findOne({username: "deni"}, (err, foundItem) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log(foundItem);
+    }
+})
+
+user.collection.insertOne(
+    {
+      username: "christine",
+      ordered: "christine"
+    }
+ )
+
 app.listen(3000)
