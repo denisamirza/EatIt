224148543f583140d39216f05081dfd68ea6ee83
Lazar Christine <jsun32500@gmail.com>
2021-10-31 18:01:15 +0200

    connection backend frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppServiceService } from './app-service.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  title = 'app-service';
+
+  constructor(private service : AppServiceService) { }
+
+  ngOnInit() {
+    this.getDataFromAPI();
+  }
+
+  getDataFromAPI() {
+    this.service.getData().subscribe((response) => {
+      console.log('Response from API is = ', response)
+    }, (error) => {
+      console.log('Error is = ', error);
+    })
+  }
 }
